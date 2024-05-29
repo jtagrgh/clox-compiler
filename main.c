a74786c9c2eb889d629c4ec128acb8c19848d0ff
jtagrgh <jakob.koblinsky@gmail.com>
2024-05-28 20:34:54 -0400

    Do Chapter 15

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+	initVM();
     Chunk chunk;
     initChunk(&chunk);
 
-    for (int i = 0; i < 260; i++) {
-        writeConstant(&chunk, i, 123);
-    }
+	writeConstant(&chunk, 1.2, 123);
+	writeConstant(&chunk, 3.4, 123);
+	writeChunk(&chunk, OP_ADD, 123);
+
+	writeConstant(&chunk, 5.6, 123);
+	writeChunk(&chunk, OP_DIVIDE, 123);
+
+	writeChunk(&chunk, OP_NEGATE, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
+	interpret(&chunk);
 
-    disassembleChunk(&chunk, "test chunk");
-
+	freeVM();
     freeChunk(&chunk);
     return 0;
 }
